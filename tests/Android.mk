5020733a3b5cb69dd58ecd447d74b48e2616204d
xc112lg <dtiven13@gmail.com>
2024-01-29 12:24:14 +0800

    Update Android.mk

--- tests/Android.mk
+++ tests/Android.mk
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ALLOW_MISSING_DEPENDENCIES=true
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
